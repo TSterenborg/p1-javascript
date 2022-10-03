dd02d193720b8cc2b7f4583080306678f2cc63f0
Tsterenborg <9018467@student.zadkine.nl>
2022-10-03 13:08:39 +0200

    6.2

---
+++ opdracht_6.2/app.js
@@ -0,0 +1,9 @@
+function kleur_aanpassen1() {
+    document.getElementById("buttontext").style.color = "red";
+    document.getElementById("buttontext").innerHTML = "Inhoud aangepast!"
+}
+
+function kleur_aanpassen2() {
+    document.getElementById("clicktext").style.color = "red";
+    document.getElementById("clicktext").innerHTML = "Inhoud aangepast!"
+}
