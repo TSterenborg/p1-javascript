462ab36439e657ee009bf6cb08dd9621c731c047
Tsterenborg <9018467@student.zadkine.nl>
2022-10-03 12:59:18 +0200

    6.1

---
+++ opdracht_6.1/app.js
@@ -0,0 +1,5 @@
+function kleur_aanpassen() {
+    document.getElementById("h1-text").style.color = "red";
+    document.getElementById("h1-text").innerHTML = "Inhoud aangepast!"
+}
+kleur_aanpassen()
